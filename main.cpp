fb93aa8800c0c21486426541da9fb9fedcc889f3
lrks <yuke1222@gmail.com>
2015-10-26 14:37:42 +0900

    もう終わりだぁ！

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@
 #define UNDEF_NOTE  128
 #define UNDEF_CH    0xff
 #define STR_BUF_SIZE    32
-#define DEMO_INTERVAL   500
 
 struct Slot {
     uint8_t ch;
@@ -21,8 +20,6 @@ struct Slot {
 Slot GLOBAL_SLOT[3];
 int GLOBAL_STATE = 0;
 uint8_t GLOBAL_RUNNING = 0x00;
-bool GLOBAL_IS_ACCEPT_DEMO = false;
-bool GLOBAL_IS_RUNNING_DEMO = false;
 
 DigitalOut red(dp27);
 DigitalOut yellow(dp5);
@@ -30,8 +27,6 @@ DigitalOut green(dp18);
 YMZ294 ymz(dp1, dp2, dp4, dp6, dp9, dp10, dp11, dp13, dp26, dp25, dp17);
 RawSerial sp(dp16, dp15); // tx, rx
 InterruptIn crash(dp28);
-Ticker dog;
-Timer fuwafuwatime;
 
 
 /*------------------------------------*/
@@ -55,8 +50,6 @@ int findCh(uint8_t midi_ch = UNDEF_CH, uint8_t note = UNDEF_NOTE);
 /*       Demo and KONOYONOOWARI       */
 /*------------------------------------*/
 void resetHandler();
-void watch();
-void demoHandler();
 
 /*------------------------------------*/
 /*              Use Xtal              */
@@ -135,15 +128,7 @@ int main() {
     crash.fall(&resetHandler);
     NVIC_SetPriority(EINT3_IRQn, 5);
 
-    // Demo
-    dog.attach(&watch, DEMO_INTERVAL);
-    NVIC_SetPriority(TIMER_16_0_IRQn, 10);
-    NVIC_SetPriority(TIMER_16_1_IRQn, 11);
-    NVIC_SetPriority(TIMER_32_0_IRQn, 12);
-    NVIC_SetPriority(TIMER_32_1_IRQn, 13);
-
     // MIDI-IN
-    fuwafuwatime.start();
     sp.attach(&uartHandler, RawSerial::RxIrq);
 }
 
@@ -164,10 +149,6 @@ void reset(bool full) {
     GLOBAL_STATE = 0;
     if (full) GLOBAL_RUNNING = 0x00;
     for (int i=0; i<3; i++) turnOff(i);
-
-    fuwafuwatime.reset();
-    GLOBAL_IS_ACCEPT_DEMO = false;
-    GLOBAL_IS_RUNNING_DEMO = false;
 
     ymz.reset();
     ymz.setMixer(NONE, NONE, NONE, TONE_C, TONE_B, TONE_A);
@@ -215,13 +196,6 @@ void uartHandler() {
     #ifdef DEBUG
         sp.puts("----\r\n");
     #endif
-
-    // for Demo
-    if (GLOBAL_IS_RUNNING_DEMO) {
-        reset(false);
-    } else {
-        fuwafuwatime.reset();
-    }
 
     // Recieve
     uint8_t recv = sp.getc();
@@ -351,257 +325,3 @@ void resetHandler() {
     reset();
 }
 
-void watch() {
-    if (GLOBAL_IS_RUNNING_DEMO) return;
-    if (fuwafuwatime.read() < DEMO_INTERVAL) return;
-
-    GLOBAL_IS_ACCEPT_DEMO = true;
-    GLOBAL_IS_RUNNING_DEMO = true;
-
-    demoHandler();
-    if (GLOBAL_IS_RUNNING_DEMO) reset(false);
-}
-
-void sound(int num);
-void sort(int num);
-void demoHandler() {
-    if (!GLOBAL_IS_ACCEPT_DEMO) return;
-
-    wait(5);
-
-    #ifdef DEBUG
-        char str[STR_BUF_SIZE];
-        sp.puts("[demoHandler]\r\n");
-    #endif
-
-    static bool flg = false;
-    if (!flg) {
-        srand(fuwafuwatime.read_us());
-        flg = true;
-    }
-
-    int r = rand() % 10;
-    #ifdef DEBUG
-        snprintf(str, STR_BUF_SIZE, "r = %d\r\n", r);
-        sp.puts(str);
-    #endif
-
-    if (r < 8) {
-        sound(r);
-    } else {
-        sort(r - 8);
-    }
-
-    #ifdef DEBUG
-        snprintf(str, STR_BUF_SIZE, "demoEnd, f1=%d\r\n", is_accept_demo);
-        sp.puts(str);
-    #endif
-
-    demoHandler();
-}
-
-void sound(int num) {
-    // Knight
-    uint8_t knight_note[] = {
-        #include "midi/knight_note.txt"
-        0xff
-    };
-
-    float knight_duration[] = {
-        #include "midi/knight_duration.txt"
-        -1.0
-    };
-
-    // Kokyou
-    uint8_t kokyou_note[] = {
-        #include "midi/kokyou_note.txt"
-        0xff
-    };
-
-    float kokyou_duration[] = {
-        #include "midi/kokyou_duration.txt"
-        -1.0
-    };
-
-    // jumper
-    uint8_t jumper_note[] = {
-        #include "midi/jumper_note.txt"
-        0xff
-    };
-
-    float jumper_duration[] = {
-        #include "midi/jumper_duration.txt"
-        -1.0
-    };
-
-    // polargiest
-    uint8_t polargiest_note[] = {
-        #include "midi/polargiest_note.txt"
-        0xff
-    };
-
-    float polargiest_duration[] = {
-        #include "midi/polargiest_duration.txt"
-        -1.0
-    };
-
-    // toe
-    uint8_t toe_note[] = {
-        #include "midi/toe_note.txt"
-        0xff
-    };
-
-    float toe_duration[] = {
-        #include "midi/toe_duration.txt"
-        -1.0
-    };
-
-    // xstep
-    uint8_t xstep_note[] = {
-        #include "midi/xstep_note.txt"
-        0xff
-    };
-
-    float xstep_duration[] = {
-        #include "midi/xstep_duration.txt"
-        -1.0
-    };
-
-    // touhoku
-    uint8_t touhoku_note[] = {
-        #include "midi/touhoku_note.txt"
-        0xff
-    };
-
-    float touhoku_duration[] = {
-        #include "midi/touhoku_duration.txt"
-        -1.0
-    };
-
-    // jouetsu
-    uint8_t jouetsu_note[] = {
-        #include "midi/jouetsu_note.txt"
-        0xff
-    };
-
-    float jouetsu_duration[] = {
-        #include "midi/jouetsu_duration.txt"
-        -1.0
-    };
-
-    uint8_t *note_array[] = {
-        knight_note,
-        kokyou_note,
-        jumper_note,
-        polargiest_note,
-        toe_note,
-        xstep_note,
-        touhoku_note,
-        jouetsu_note,
-    };
-
-    float *duration_array[] = {
-        knight_duration,
-        kokyou_duration,
-        jumper_duration,
-        polargiest_duration,
-        toe_duration,
-        xstep_duration,
-        touhoku_duration,
-        jouetsu_duration,
-    };
-
-    int id = num % 8;
-    uint8_t *note_p = note_array[id];
-    float *duration_p = duration_array[id];
-
-    for (int i=0; (note_p[i >> 1] != 0xff && duration_p[i] != -1.0); i++) {
-        if (!GLOBAL_IS_ACCEPT_DEMO) return;
-        if (i % 2 == 0) {
-            ymz.setVolume(CHANNEL_A, 0xf);
-            turnOn(0, 0x0, note_p[i >> 1]);
-            yellow = 1;
-            green = 1;
-        } else {
-            turnOff(0);
-            yellow = 0;
-            green = 0;
-        }
-        wait(duration_p[i]);
-    }
-
-    turnOff(0);
-    yellow = 0;
-    green = 0;
-}
-
-bool isSoted(uint8_t *data, int n) {
-    for (int i=0; i<(n-1); i++) {
-        if (data[i] > data[i+1]) return false;
-    }
-    return true;
-}
-
-void swap(uint8_t *n1, uint8_t *n2) {
-    uint8_t tmp = *n1;
-    *n1 = *n2;
-    *n2 = tmp;
-
-    ymz.setVolume(CHANNEL_A, 0xf);
-    turnOn(0, 0x0, *n1);
-    yellow = 1;
-    green = 1;
-
-    wait_ms(150);
-
-    turnOff(0);
-    yellow = 0;
-    green = 0;
-
-    wait_ms(150);
-}
-
-void bogoSort(uint8_t *data, int n) {
-    while (!isSoted(data, n)) {
-        if (!GLOBAL_IS_ACCEPT_DEMO) return;
-        int i1, i2 = rand() % n;
-        do {
-            i1 = rand() % n;
-        } while (i1 == i2);
-        swap(&data[i1], &data[i2]);
-    }
-}
-
-void quickSort(uint8_t *data, int left, int right) {
-    int pivot = data[(left + right) >> 1];
-    int l = left, r = right;
-
-    while (1) {
-        if (!GLOBAL_IS_ACCEPT_DEMO) return;
-        while ((data[l] < pivot) && (l < right)) l++;
-        while ((data[r] > pivot) && (left < r)) r--;
-        if (l > r) break;
-        swap(&data[l++], &data[r--]);
-    }
-
-    if (left < r) quickSort(data, left, r);
-    if (l < right) quickSort(data, l, right);
-}
-
-void sort(int num) {
-    #define QUICK_DATA_SIZE 100
-    #define BOGO_DATA_SIZE  5
-    uint8_t data[QUICK_DATA_SIZE];  // QUICK_DATA_SIZE >= BOGO_DATA_SIZE
-
-    switch(num) {
-    case 0:
-        for (int i=0; i<BOGO_DATA_SIZE; i++) data[i] = 50 + (rand() % 50);
-        bogoSort(data, BOGO_DATA_SIZE);
-        break;
-    case 1:
-    default:
-        for (int i=0; i<BOGO_DATA_SIZE; i++) data[i] = 40 + (rand() % 60);
-        quickSort(data, 0, QUICK_DATA_SIZE - 1);
-        break;
-    }
-}
